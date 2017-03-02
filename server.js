9801505b8c446c3a8c9d7b392c6cebf5f58cd63b
rivermanvr <vrgithub@riversconsulting.com>
2017-03-02 18:29:49 -0500

    created server & app.js, changed .json

---
+++ server.js
@@ -0,0 +1,11 @@
+const server = require( 'http' ).createServer(require( './app' ));
+const acmeDB = require( './db' );
+
+tripDB.seed()
+    .then(() => console.log('your data is seeded'))
+    .catch( err => console.log(err));
+
+console.log('Models: ', tripDB.models);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`listening on port ${port}`));
