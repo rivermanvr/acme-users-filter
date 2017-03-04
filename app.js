ae7009e8fee0f6099e85653fd96f5e8f7ab49ed5
rivermanvr <vrgithub@riversconsulting.com>
2017-03-03 21:42:42 -0500

    entered root route

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require( 'express' );
 const app = express();
 const faker = require( 'faker' );
+
+app.get('/', (req, res, next) => {
+    res.send(console.log('hello'));
+});
+
 module.exports = app;
