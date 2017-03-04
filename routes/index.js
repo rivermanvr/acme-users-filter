547d4341a6be2ca36116f162b2ecb30336176188
rivermanvr <vrgithub@riversconsulting.com>
2017-03-04 13:56:03 -0500

    created models require

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require( 'express' );
 const router = express.Router();
+const models = require( '../db' ).models;
 
 router.get('/', (req, res, next) => {
-    res.render('index');
+    res.render('index', {title: 'Home'});
 });
 
 module.exports = router;
