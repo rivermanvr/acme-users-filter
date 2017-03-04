348ddd91c03520f5d34cc0cab0cf68f1b983d5dd
rivermanvr <vrgithub@riversconsulting.com>
2017-03-04 01:01:45 -0500

    changed progress db name

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require( 'sequelize' );
 
-const connectDB = process.env.DATABASE_URL || 'postgres://localhost/acmeDB';
+const connectDB = process.env.DATABASE_URL || 'postgres://localhost/acme_sql';
 const acmeDB = new Sequelize(connectDB, {
     logging: false
 })
