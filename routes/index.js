341fc26f961e34c9cdff392eb035a9420237b5d5
rivermanvr <vrgithub@riversconsulting.com>
2017-03-07 15:10:56 -0500

    new improved routes done

--- routes/index.js
+++ routes/index.js
@@ -11,36 +11,21 @@ router.use('/', (req, res, next) => {
 });
 
 router.get('/', (req, res, next) => {
-    let userRecords, userIndex;
     models.User.getUsers()
-        .then((_userRecords) => {
-            userRecords = _userRecords;
-            return models.User.mapLastNm();
-        })
-        .then((sortedKeys) => {
+        .then((userRecords) => {
             res.render('index', {title: 'Home', nav: 'all', users: userRecords});
         })
         .catch((err) => console.log(err));
 });
 
-// router.get('/users/filter/:letter', (req, res, next) => {
-//     let letterFilter = req.params.letter, sortedKeys;
-//     models.User.getUsers()
-//         .then((_userRecords) => {
-//             return acmeDB.mapLastNm(_userRecords);
-//         })
-//         .then((_userIndex) => {
-//             return acmeDB.sortKeys(_userIndex);
-//         })
-//         .then((_sortedKeys) => {
-//             sortedKeys = _sortedKeys;
-//             return models.User.getUsers(letterFilter)
-//         })
-//         .then((userRecords) => {
-//             res.render('index', {title: 'Acme Filter', nav: letterFilter, users: userRecords, sortedKeys: sortedKeys});
-//         })
-//         .catch((err) => console.log(err));
-// });
+router.get('/users/filter/:letter', (req, res, next) => {
+    let letterFilter = req.params.letter;
+    models.User.getUsers(letterFilter)
+        .then((userRecords) => {
+            res.render('index', {title: 'Acme Filter', nav: letterFilter, users: userRecords});
+        })
+        .catch((err) => console.log(err));
+});
 
 router.post('/regenerate', (req, res, next) => {
     acmeDB.seed()
