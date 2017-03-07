95aa3b4d22959a34cd97e83b7eefb23a2fddfa05
rivermanvr <vrgithub@riversconsulting.com>
2017-03-07 13:36:52 -0500

    changed server.js: gave it more clarity.

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const server = require( 'http' ).createServer(require( './app' ));
+const http = require( 'http' );
+const app = require( './app' );
 const acmeDB = require( './db' );
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
 
-acmeDB.seed()
+acmeDB.seed(true)
     .then(() => console.log('your data is seeded'))
     .catch(err => console.log(err));
-
-const port = process.env.PORT || 3000;
 
 server.listen(port, () => console.log(`listening on port ${port}`));
